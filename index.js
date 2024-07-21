6d39c5a0d8d7f7144d1460e03188cb6ee408fa41
Gowtham <gowtham@aracreate.com>
2024-07-21 14:14:31 +0530

    feat: merging the features (#4)
    * chore(release): 1.0.0-develop.1 [skip ci]
    
    # 1.0.0-develop.1 (2024-07-21)
    
    ### Bug Fixes
    
    * add proper yml file to the repo ([739b844](https://github.com/gowtham-aracreate/semantic-release-playground/commit/739b844ac18438f49f5184656a069b218ac49185))
    * try make github actions to work ([3fc3f57](https://github.com/gowtham-aracreate/semantic-release-playground/commit/3fc3f5770e3bae6d53ed40b34ca5bf2f831556e7))
    
    * feat(api): add welcome api
    
    * feat(api): add hello api (#2)
    
    * Feature/1 feat add welcome api (#3)
    
    * feat(api): add hello api
    
    * chore: modify releasesrc file
    
    * chore(release): 1.0.0-develop.2 [skip ci]
    
    # [1.0.0-develop.2](https://github.com/gowtham-aracreate/semantic-release-playground/compare/v1.0.0-develop.1...v1.0.0-develop.2) (2024-07-21)
    
    ### Features
    
    * **api:** add hello api ([#2](https://github.com/gowtham-aracreate/semantic-release-playground/issues/2)) ([348c961](https://github.com/gowtham-aracreate/semantic-release-playground/commit/348c96126a1c68e188ecdd610e54571b62044e29))
    * **api:** add welcome api ([3d39aa1](https://github.com/gowtham-aracreate/semantic-release-playground/commit/3d39aa18c4816d6651c54afc2c8748afd7bbc98d))
    
    * chore(release): 1.1.0-develop.1 [skip ci]
    
    # [1.1.0-develop.1](https://github.com/gowtham-aracreate/semantic-release-playground/compare/v1.0.0...v1.1.0-develop.1) (2024-07-21)
    
    ### Features
    
    * **api:** add hello api ([#2](https://github.com/gowtham-aracreate/semantic-release-playground/issues/2)) ([348c961](https://github.com/gowtham-aracreate/semantic-release-playground/commit/348c96126a1c68e188ecdd610e54571b62044e29))
    * **api:** add welcome api ([3d39aa1](https://github.com/gowtham-aracreate/semantic-release-playground/commit/3d39aa18c4816d6651c54afc2c8748afd7bbc98d))
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.json({ message: "hello Guys" }))
 
+app.get("/welcome", (req, res) => res.json({ message: 'welcome you all!!!!' }))
+
+app.get("/hello", (req, res) => res.json({ message: 'hello you all!!!!' }))
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });

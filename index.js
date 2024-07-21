86335aa578502676772a3a40c561a42b519cf267
gowtham-aracreate <gowtham@aracreate.com>
2024-07-21 13:24:59 +0530

    feat(api): add hello api

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get("/", (req, res) => res.json({ message: "hello Guys" }))
 
 app.get("/welcome", (req, res) => res.json({ message: 'welcome you all!!!!' }))
 
+app.get("/hello", (req, res) => res.json({ message: 'hello you all!!!!' }))
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });

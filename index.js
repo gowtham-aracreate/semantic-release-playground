0ce4520a0d60460dfe7b4342a892497a026ba21c
gowtham-aracreate <gowtham@aracreate.com>
2024-07-21 15:35:56 +0530

    feat: add multiply & division APIs
    - Added `multiply` API for performing multiplication operations.
    
    Related issue: #14

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.get("/sub", (req, res) => res.send({ message: `${4 - 3}` }))
 
 app.get('/add', (req, res) => res.send("4 + 2 = 1, its a addition"));
 
+app.get('/mul', (req, res) => res.send("4 * 2 = 8, its a Multiply"));
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
